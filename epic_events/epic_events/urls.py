4aa7ed6f0d143194287646312457609e05508033
chrisopenclass <90398020+chrisopenclass@users.noreply.github.com>
2022-07-29 18:40:02 +0200

    ajout des différentes route d'url

--- epic_events/epic_events/urls.py
+++ epic_events/epic_events/urls.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
-#from clients.views import ClientViewSet
-#from events.views import EventViewSet
-#from contracts.views import ContractViewSet
+from clients.views import ClientViewSet
+from events.views import EventViewSet
+from contracts.views import ContractViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
-"""
+
 router = SimpleRouter()
 
 router.register(r'clients', ClientViewSet, basename='clients')
 router.register(r'contracts', ContractViewSet, basename="contracts")
-router.register(r'events', EventViewSet, basename='events')"""
+router.register(r'events', EventViewSet, basename='events')
 
 
 urlpatterns = [
@@ -22,5 +22,5 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api-auth/', include('rest_framework.urls')),
-    #path(r'', include(router.urls)),
+    path(r'', include(router.urls)),
 ]
