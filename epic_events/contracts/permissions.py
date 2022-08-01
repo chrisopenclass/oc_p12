bb267f4d736ad591adc74fcc374e74073d9faef2
chrisopenclass <90398020+chrisopenclass@users.noreply.github.com>
2022-08-01 22:26:41 +0200

    ajout des permissions pour les contrats

---
+++ epic_events/contracts/permissions.py
@@ -0,0 +1,23 @@
+from rest_framework import permissions
+
+
+class IsSaleEmployeeAssigned(permissions.BasePermission):
+    message = "You can update a contract only if you're assigned to it"
+
+    def has_permission(self, request, view):
+        return request.user
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.client.sales_contact == request.user
+
+
+class IsSupportEmployee(permissions.BasePermission):
+    message = "Support employee can only read data"
+
+    def has_permission(self, request, view):
+        if request.method == 'POST' and request.user.role == 'support':
+            return False
+        return True
--- epic_events/contracts/views.py
+++ epic_events/contracts/views.py
@@ -2,11 +2,12 @@ from rest_framework import viewsets
 from .models import Contract
 from .serializers import ContractSerializer
 from rest_framework.permissions import IsAuthenticated
+from .permissions import IsSaleEmployeeAssigned, IsSupportEmployee
 
 
 class ContractViewSet(viewsets.ModelViewSet):
     serializer_class = ContractSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsSaleEmployeeAssigned, IsSupportEmployee]
 
     def get_queryset(self):
         return Contract.objects.all()
